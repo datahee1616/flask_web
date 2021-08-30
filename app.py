fb55806f9ac7fc948520180e51db5d197f068080
Sanghee Kim <sanghee@saenghi-MacBookPro.local>
2021-08-30 16:07:01 +0900

    articles.html

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from data import Articles
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+# @ : 함수를 처리해줌
+# .route : 중개역할
+
+@app.route('/', methods=['Get', 'POST']) # get방식이던 post방식이던 여기로 들어오면 다음 함수를 처리해라
+def index():
+    name="KIM"
+    return render_template('index.html', data=name) # 가져오고자 하는 html 파일 가져오기
+
+@app.route('/articles', methods=['Get','Post']) # articles로 요청을 날림
+def articles():
+    list_data = Articles()
+    return render_template('articles.html', data=list_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
